2bcf839bd614497878190d45e1b5e20fd119e294
Psol <upsol@protonmail.com>
2023-09-17 11:32:52 +0200

    werks

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+if not exist .\\bin mkdir .\\bin
+set exe-name=blanky.exe
+set cflags=/W3 /WX 
+set sources=main.cpp
+set headers="%USERPROFILE%/vcpkg/installed/x64-windows/include"
+set raylib-path="%USERPROFILE%/vcpkg/installed/x64-windows/lib/raylib.lib"
+set raylib-dll-path="%USERPROFILE%\vcpkg\installed\x64-windows\bin\raylib.dll"
+set libs=kernel32.lib user32.lib shell32.lib winmm.lib gdi32.lib opengl32.lib
+cl.exe %cflags% /I%headers% %sources% /Fobin\ /Febin\%exe-name% %libs% %raylib-path%
+copy %raylib-dll-path% .\\bin\raylib.dll
